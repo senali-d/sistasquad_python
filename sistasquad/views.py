0d1c4ba7e4ebfceb24e09dd091627835633fe666
Senali <senalidilumika@gmail.com>
2023-03-05 02:40:05 +0530

    Implement djangorestframework

--- sistasquad/views.py
+++ sistasquad/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+@api_view(["GET"])
+def getProfile(request):
+  return Response(status=status.HTTP_200_OK)
