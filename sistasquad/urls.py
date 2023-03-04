830181b357156952d13465e7881e220a3fc83683
Senali <senalidilumika@gmail.com>
2023-03-05 03:32:51 +0530

    Created sign in and sign up api

--- sistasquad/urls.py
+++ sistasquad/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 urlpatterns = [
+  path('signin', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+  path('signup', views.RegisterView.as_view(), name='auth_register'),
+
   path('profile', views.getProfile),
 ]
--- sistasquad/views.py
+++ sistasquad/views.py
@@ -1,7 +1,22 @@
+from django.contrib.auth.models import User
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import TokenObtainPairSerializer, RegisterSerializer
+
+class MyTokenObtainPairView(TokenObtainPairView):
+  serializer_class = TokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  permission_classes = (AllowAny,)
+  serializer_class = RegisterSerializer
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getProfile(request):
   return Response(status=status.HTTP_200_OK)
